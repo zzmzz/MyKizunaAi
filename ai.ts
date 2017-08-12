c097dfb0e0ce37874edc8a29b366832a5607d707
朱子沫 <StevenZhu@zhuzimodeMacBook-Pro-2.local>
2017-08-12 13:01:37 +0800

    ai

--- ai.ts
+++ ai.ts
@@ -43,7 +43,7 @@ bot
         log.info('Bot', `${user.name()} logined`)
         this.say('wechaty contact-bot just logined')
 
-        schedule.scheduleJob('* * 12 * * *', main.bind(this));
+        schedule.scheduleJob('* * 13 * * *', main.bind(this));
     })
     .on('logout', user => log.info('Bot', `${user.name()} logouted`))
     .on('error', e => log.info('Bot', 'error: %s', e))
